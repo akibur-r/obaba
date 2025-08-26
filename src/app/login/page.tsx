5ee5994edd06662f322c1a78e67738993c951865
akibur-r <akibur.pciu@gmail.com>
2025-08-27 00:52:31 +0600

    Updated sign in component to redirect after success

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,10 +3,14 @@ import { Button } from "@/components/ui/button";
 import { signIn } from "next-auth/react";
 
 export default function LoginPage() {
+  const handleGoogleSignIn = async () => {
+    await signIn("google", { callbackUrl: "/products" });
+  };
+
   return (
     <div className="flex flex-col items-center justify-center py-32">
       <h1 className="text-3xl font-bold mb-6">Login</h1>
-      <Button onClick={() => signIn("google")}>Sign in with Google</Button>
+      <Button onClick={handleGoogleSignIn}>Sign in with Google</Button>
     </div>
   );
 }
