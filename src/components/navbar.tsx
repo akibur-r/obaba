0162f369905addb1872eb473daeeedece3c8b83f
akibur-r <akibur.pciu@gmail.com>
2025-08-27 00:30:25 +0600

    Updated add product link styles in navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,6 +5,7 @@ import { Button } from "@/components/ui/button";
 import { signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
+import { FaPlus } from "react-icons/fa";
 
 export default function Navbar() {
   const { data: session } = useSession();
@@ -19,20 +20,26 @@ export default function Navbar() {
           Products
         </Link>
         <nav className="flex gap-4 items-center">
-          <Link href="/dashboard/add-product" className="hover:text-blue-600">
-            Add Product
-          </Link>
           {!session ? (
             <Link href="/login" className="px-3 py-1 border rounded">
               Login
             </Link>
           ) : (
-            <Button
-              onClick={() => signOut()}
-              className="px-3 py-1 border rounded"
-            >
-              Logout
-            </Button>
+            <div className="flex items-center gap-2">
+              <Link
+                href="/dashboard/add-product"
+                className="hover:text-blue-600"
+              >
+                <span className="hidden md:block">Add Product</span>
+                <FaPlus className="md:hidden" />
+              </Link>
+              <Button
+                onClick={() => signOut()}
+                className="px-3 py-1 border rounded"
+              >
+                Logout
+              </Button>
+            </div>
           )}
         </nav>
       </div>
